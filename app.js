5eaf0468857dffbf8fd0e08ed2b1ee277fb8acf3
Mharycell <mharycell@hotmail.com>
2022-08-13 16:06:42 -0500

    creacion de carpetas

--- app.js
+++
@@ -1,6 +0,0 @@
-var age = parseInt(prompt("¿Cuál es tu edad?")); //33
-//Convirtiendo edad a segundos
-var ageInSeconds = age * 365 * 24 * 60 * 60;
-console.log(ageInSeconds)
-    //Mostrando edad en la web
-document.write("Tu edad es " + age + " y en segundos sería " + ageInSeconds);
